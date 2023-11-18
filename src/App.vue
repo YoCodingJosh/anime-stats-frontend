a27b8fd6dfb0a4a31002398b31bf75aa43e5e525
Josh Kennedy <976307+YoCodingJosh@users.noreply.github.com>
2023-11-18 17:15:19 -0600

    some styling and layout work

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
 
 import { ref } from 'vue'
 
@@ -15,8 +14,16 @@ export default {
 
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer">
-      <!--  -->
+    <v-navigation-drawer v-model="drawer" class="bg-deep-purple-lighten-1" theme="dark">
+      <v-list color="transparent">
+        <v-list-item link title="Home" to="/"></v-list-item>
+        <v-list-item link title="About" to="/about"></v-list-item>
+      </v-list>
+      <template v-slot:append>
+        <div class="pa-2 ">
+          <h2 class="text-center">（＾ｖ＾）</h2>
+        </div>
+      </template>
     </v-navigation-drawer>
 
     <v-app-bar>
@@ -26,84 +33,24 @@ export default {
     </v-app-bar>
 
     <v-main>
-      <header>
-        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-        <div class="wrapper">
-          <HelloWorld msg="You did it!" />
-
-          <nav>
-            <RouterLink to="/">Home</RouterLink>
-            <RouterLink to="/about">About</RouterLink>
-          </nav>
-        </div>
-      </header>
-
       <RouterView />
     </v-main>
+
+    <v-footer app border>
+      <span>
+        &copy; {{ new Date().getFullYear() }} CodingJosh
+        &mdash;
+        Privacy Policy
+        &bull;
+        Terms of Service
+      </span>
+    </v-footer>
   </v-app>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.v-list-item--active {
+  color: white !important; /* Change this to whatever color you want */
+  background-color: deep-purple !important; /* Change this to whatever color you want */
 }
 </style>
