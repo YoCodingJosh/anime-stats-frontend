8511c199c24744d2b35c9c26265c9bdca36e49d1
Josh Kennedy <976307+YoCodingJosh@users.noreply.github.com>
2023-11-22 17:21:58 -0600

    Add basic username entry

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container>
+    <div class="about">
+      <h1>This is an about page</h1>
+    </div>
+  </v-container>
 </template>
 
-<style>
-</style>
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,53 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue'
+
+const username = ref('');
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL as string;
+
+const healthCheck = async () => {
+  try {
+    const response = await fetch(`${backendUrl}/health`);
+    if (response.status !== 200) {
+      throw new Error('Backend is down');
+    }
+  } catch (error) {
+    console.error(error);
+    // TODO: Show error message
+  }
+}
+
+healthCheck();
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <v-container>
+    <header>
+      <h1>Welcome!</h1>
+    </header>
+    <main>
+      <p>
+        This is the home page. You can edit the content of this page by editing
+        <code>src/views/HomeView.vue</code>.
+      </p>
+      <br />
+      <div class="d-flex align-center flex-column">
+        <v-card width="450">
+          <template v-slot:title>
+            Let's do this!
+          </template>
+
+          <template v-slot:subtitle>
+            Enter your username and let's start crunching some numbers!
+          </template>
+
+          <template v-slot:text>
+            <v-text-field v-model="username" label="Username" outlined dense placeholder="CodingJosh"></v-text-field>
+            <v-btn block class="text-none mb-4" color="indigo-darken-1" size="x-large" variant="flat"
+              @click="() => $router.push(`/user/${username}`)">Go</v-btn>
+          </template>
+        </v-card>
+      </div>
+    </main>
+  </v-container>
 </template>
