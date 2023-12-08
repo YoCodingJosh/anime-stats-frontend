ded4de92d1caeefd1a0433e3e2496f5c450fd386
Josh Kennedy <976307+YoCodingJosh@users.noreply.github.com>
2023-12-08 15:06:03 -0600

    improvments of animegif component

--- src/components/AnimeGif.vue
+++ src/components/AnimeGif.vue
@@ -1,46 +1,92 @@
 <script setup lang="ts">
-import { onMounted, ref, defineProps } from 'vue';
+import { onMounted, ref } from 'vue';
 import type GifsJson from './AnimeGifData';
+import type { AnimeDetail } from './AnimeGifData';
 
 import data from '@/assets/gifs.json';
 
 const props = defineProps({
+  /**
+   * The key of the image to display.
+   */
   imageKey: {
     type: String,
     required: false,
   },
 
+  /**
+   * The tag to use to get a random image from that set.
+   */
   applicableTag: {
     type: String,
     required: false,
   },
 
-  maxWidth: {
-    type: String,
+  /**
+   * Whether to show the anime icon or not. The anime icon is a thumbnail of the anime image from MyAnimeList.
+   * 
+   * Note: This will cause an additional request to MyAnimeList/Jikan. Only one request if combined with `showAnimeDetails` ;)
+   */
+  showAnimeIcon: {
+    type: Boolean,
     required: false,
-    default: '100%',
+    default: true,
   },
 
-  maxHeight: {
-    type: String,
+  /**
+   * Whether to show the watch button. The watch button will take the user to an offical streaming site (Crunchyroll or HIDIVE)
+   */
+  showWatchButton: {
+    type: Boolean,
     required: false,
-    default: '100%',
+    default: false,
+  },
+
+  /**
+   * Show the anime details like genres, release date, format, etc.
+   * 
+   * Note: This will cause an additional request to MyAnimeList/Jikan. Only one request if combined with `showAnimeIcon` ;)
+   */
+  showAnimeDetails: {
+    type: Boolean,
+    required: false,
+    default: false,
   },
 });
 
 const imageUrl = ref('');
 const imageWidth = ref(0);
 const imageHeight = ref(0);
+const animeTitle = ref('');
+const animeMalUrl = ref('');
 
 onMounted(() => {
+  if (props.imageKey && props.applicableTag) {
+    throw new Error('Cannot use both imageKey and applicableTag');
+  }
+
+  if (!props.imageKey && !props.applicableTag) {
+    throw new Error('Must use either imageKey or applicableTag');
+  }
+
   const animeGifData: GifsJson = data;
 
   // either get a random image or get the image from the key or a random image from the tag
   if (props.imageKey) {
     const image = animeGifData.gifs.find((gif) => gif.name === props.imageKey);
-    imageUrl.value = image?.file ?? '';
-    imageWidth.value = image?.width ?? 0;
-    imageHeight.value = image?.height ?? 0;
+
+    if (!image) {
+      throw new Error(`Could not find image with key ${props.imageKey}`);
+    }
+
+    imageUrl.value = image.file;
+    imageWidth.value = image.width;
+    imageHeight.value = image.height;
+
+    const animeDetail: AnimeDetail = animeGifData.anime_details[image.mal_id];
+
+    animeTitle.value = animeDetail.title;
+    animeMalUrl.value = `https://myanimelist.net/anime/${image.mal_id}`;
   } else if (props.applicableTag) {
     const applicableGifs = Object.entries(animeGifData).filter(([, tags]) => tags.includes(props.applicableTag));
     const randomApplicableGif = applicableGifs[Math.floor(Math.random() * applicableGifs.length)];
@@ -57,18 +103,16 @@ onMounted(() => {
 </script>
 
 <template>
-  <v-card>
+  <v-card :max-width="imageWidth">
     <v-img :src="`/gifs/${imageUrl}`" :max-width="imageWidth" :max-height="imageHeight"></v-img>
+    <v-card-title>
+      <a :href="animeMalUrl" target="_blank" rel="noopener noreferrer">{{ animeTitle }}</a>
+    </v-card-title>
+
+    <v-card-subtitle>
+      genres go here
+    </v-card-subtitle>
   </v-card>
-
-  <v-card-title>
-    TODO:
-  </v-card-title>
-
-  <v-card-subtitle>
-    also TODO:
-  </v-card-subtitle>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
