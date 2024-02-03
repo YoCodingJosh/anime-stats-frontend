3fc7ea5f284e8b01bc0aaf7f7a248e17b1f82863
Josh Kennedy <976307+YoCodingJosh@users.noreply.github.com>
2024-02-02 21:45:14 -0600

    work on watch status ring

--- src/stats/StatusDonut.vue
+++ src/stats/StatusDonut.vue
@@ -1,10 +1,33 @@
 <template>
   <v-card>
-
+    <Doughnut :data="data" :options="options" />
   </v-card>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'vue-chartjs';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const data = ref({
+  labels: ['Watching', 'Completed', 'On Hold', 'Dropped', 'Plan to Watch'],
+  datasets: [
+    {
+      label: 'Anime Status',
+      data: [300, 50, 100, 40, 120], // TODO: replace with real data from the statsData store
+      backgroundColor: ['#338543', '#2d4276', '#c9a31f', '#832f30', '#747474'],
+    },
+  ],
+});
+
+const options = ref({
+  responsive: true,
+  maintainAspectRatio: false,
+});
+
 </script>
 
 <style scoped>
